79645ba63d247d09f50d396936a1b426b0b6e001
Sebastian Nöll <mail@sebastiannoell.de>
2019-01-01 18:44:50 +0100

    refactored

---
+++ functional/reduce.js
@@ -0,0 +1,16 @@
+// reduce vs imperative
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+// Imperative approach
+function sumOfElements(array) {
+  let total = 0;
+  array.forEach(element => {
+    return (total += element);
+  });
+  return total;
+}
+// reduce alternative
+let totalAlt = numbers.reduce((total, el) => total += el);
+
+console.log("Ergebnis: ", sumOfElements(numbers));
+console.log("Ergebnis mit reduce(): ", totalAlt);
