99f0623ca7b1364abb785d6c1c590c38ef940b93
Sebastian Nöll <mail@sebastiannoell.de>
2019-01-01 16:00:01 +0100

    refactored

--- day/day.js
+++ day/day.js
@@ -1,3 +1,4 @@
+// Funtion to return the name of the weekday for the given date
 function dayName(date) {
   const daysOfTheWeek = [
     "Sonntag",
@@ -10,4 +11,10 @@ function dayName(date) {
     "Sonntag"
   ];
   return daysOfTheWeek[date.getDay()];
+}
+
+// Returns a greeting msg with a given date
+function greeting(user, date) {
+  user = user.trim(); // Trimming them user
+  return `Moin ${user}, heute ist ${dayName(date)}.`;
 }
