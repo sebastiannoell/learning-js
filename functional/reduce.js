bf098f32e95eeec9775642d805af03b51c4f4bec
Sebastian Nöll <mail@sebastiannoell.de>
2019-01-01 22:36:17 +0100

    just some info on the init val

--- functional/reduce.js
+++ functional/reduce.js
@@ -1,3 +1,4 @@
+// Example 1
 // reduce vs imperative
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
@@ -10,7 +11,9 @@ function sumOfElements(array) {
   return total;
 }
 // reduce alternative
-let totalAlt = numbers.reduce((total, el) => total += el);
+let totalAlt = numbers.reduce((total, el) => total += el, 0); // 0 is default init for total
 
 console.log("Ergebnis: ", sumOfElements(numbers));
 console.log("Ergebnis mit reduce(): ", totalAlt);
+
+// Example 2
