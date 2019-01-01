36c0eedc1883cd2b8fd0f361ce358c4db88854e5
Sebastian Nöll <mail@sebastiannoell.de>
2019-01-01 15:19:19 +0100

    function to compare numbers

---
+++ compareNumbers.js
@@ -0,0 +1,12 @@
+let a = [8, 17, 42, 99];
+
+function numberCompare(a, b) {
+  if (Number(a) > Number(b)) {
+    return 1;
+    } else if (Number(a) < Number(b)) {
+      return -1;
+    } else {
+      return 0;
+    }
+}
+
