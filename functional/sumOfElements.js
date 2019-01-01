26033902d551e4f9124db0feae81aff0fb923461
Sebastian Nöll <mail@sebastiannoell.de>
2019-01-01 23:29:28 +0100

    exercise!

--- functional/sumOfElements.js
+++ functional/sumOfElements.js
@@ -1,6 +1,6 @@
 // Example 1
 // reduce vs imperative
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 // Imperative approach
 function sumOfElements(array) {
@@ -11,7 +11,15 @@ function sumOfElements(array) {
   return total;
 }
 // reduce alternative
-let totalAlt = numbers.reduce((total, el) => total += el, 0); // 0 is default init for total
+let totalAlt = numbers.reduce((total, el) => (total += el), 0); // 0 is default init for total
 
 console.log("Ergebnis: ", sumOfElements(numbers));
 console.log("Ergebnis mit reduce(): ", totalAlt);
+
+// exercise Product instead of sum
+
+function productOfElements(el) {
+  return el.reduce((total, el) => (total *= el), 1); // one sneaaakie boie
+}
+
+console.log("Produkt: ", productOfElements(numbers));
