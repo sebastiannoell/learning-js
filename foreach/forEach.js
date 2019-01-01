9fd1c48523bf20d7a22c41065d107a89d375d50d
Sebastian Nöll <mail@sebastiannoell.de>
2019-01-01 17:02:54 +0100

    doing forEach exercises

--- foreach/forEach.js
+++ foreach/forEach.js
@@ -8,10 +8,20 @@ bestArrayEver.forEach(function(e) {
 });
 
 // Exercise with fat arrow, i like
-bestArrayEver.forEach((e) => {
+bestArrayEver.forEach(e => {
   console.log(e);
-})
+});
 
 Array.from(soliloquy).forEach(function(char) {
   console.log(char);
 });
+
+// Exercise array with numbers to sort
+a = [8, 17, 42, 99, 2, 1, 0.23, 5];
+a.sort((a, b) => {
+  console.log("Logging from sort method: ", a - b);
+  return a - b;
+}).forEach(element => {
+  console.log(element);
+});
+console.log(a);
